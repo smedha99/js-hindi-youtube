ac003905b095490283aa73a787a177a7ecbeb666
smedha99 <144618806+smedha99@users.noreply.github.com>
2025-01-26 05:45:04 +0000

    conversion

---
+++ 01_basics/03_conversion.js
@@ -0,0 +1,35 @@
+let score="33"
+let num="33abc"
+
+//console.log(typeof score);
+//console.log(typeof num);
+
+let valueInNumber=Number(score);
+let valInNumber=Number(num);
+
+//console.log(typeof valueInNumber);
+//console.log(typeof valInNumber);
+
+//console.log(valInNumber);  // its value is NaN(not a number)
+//console.log(valueInNumber);
+//convert value in number
+//"33"=>33
+//"33abs"=>NaN
+//true=>1
+//false=>0
+
+let isLoggedIn=1
+let booleanIsLoggedIn=Boolean(isLoggedIn)
+//console.log(booleanIsLoggedIn);
+
+//in number conversion to boolean
+//1=>true  
+//0=>false
+
+//"hitesh"=>true
+//""=>false
+
+let someNum=33
+let stringNumber=String(someNum)
+console.log(stringNumber);
+console.log(typeof stringNumber);
