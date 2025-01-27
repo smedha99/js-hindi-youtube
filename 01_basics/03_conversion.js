cdda114a2a92e928d860990bd3ccb83f926beef0
smedha99 <144618806+smedha99@users.noreply.github.com>
2025-01-27 14:23:07 +0000

    comparion in js

--- 01_basics/03_conversion.js
+++ 01_basics/03_conversion.js
@@ -1,35 +1,67 @@
-let score="33"
-let num="33abc"
+// let score="33"
+// let num="33abc"
 
-//console.log(typeof score);
-//console.log(typeof num);
+// //console.log(typeof score);
+// //console.log(typeof num);
 
-let valueInNumber=Number(score);
-let valInNumber=Number(num);
+// let valueInNumber=Number(score);
+// let valInNumber=Number(num);
 
-//console.log(typeof valueInNumber);
-//console.log(typeof valInNumber);
+// //console.log(typeof valueInNumber);
+// //console.log(typeof valInNumber);
 
-//console.log(valInNumber);  // its value is NaN(not a number)
-//console.log(valueInNumber);
-//convert value in number
-//"33"=>33
-//"33abs"=>NaN
-//true=>1
-//false=>0
+// //console.log(valInNumber);  // its value is NaN(not a number)
+// //console.log(valueInNumber);
+// //convert value in number
+// //"33"=>33
+// //"33abs"=>NaN
+// //true=>1
+// //false=>0
 
-let isLoggedIn=1
-let booleanIsLoggedIn=Boolean(isLoggedIn)
-//console.log(booleanIsLoggedIn);
+// let isLoggedIn=1
+// let booleanIsLoggedIn=Boolean(isLoggedIn)
+// //console.log(booleanIsLoggedIn);
 
-//in number conversion to boolean
-//1=>true  
-//0=>false
+// //in number conversion to boolean
+// //1=>true  
+// //0=>false
 
-//"hitesh"=>true
-//""=>false
+// //"hitesh"=>true
+// //""=>false
 
-let someNum=33
-let stringNumber=String(someNum)
-console.log(stringNumber);
-console.log(typeof stringNumber);
+// let someNum=33
+// let stringNumber=String(someNum)
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
+
+//**************************************Operation *******************************
+
+let value=3
+let negValue=-value
+//console.log(negValue);
+
+let str1="hello"
+let str2="medha"
+let str3=str1+str2;
+// console.log(str3);
+
+// console.log("1"+2);
+// console.log(1+"2");
+// console.log("1"+2+2);
+// console.log(1+2+"2");
+// console.log(1+"2"+"3"+4+3);
+// console.log(1+7+"2"+"3"+4+3);
+
+// console.log(+true);
+// console.log(+"");
+
+// let num1,num2,num3;
+// num1=num2=num3=2+2;
+// console.table([num1,num2,num3,num1+num2+num3]);
+
+let gameCounter=100
+gameCounter++;
+console.log(gameCounter);
+++gameCounter;
+console.log(gameCounter);
+console.log(gameCounter); 
