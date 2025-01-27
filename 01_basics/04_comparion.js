df046b8bd5aac0ee0b25d524bb57608718abd527
smedha99 <144618806+smedha99@users.noreply.github.com>
2025-01-27 14:24:14 +0000

    datatypes in js

---
+++ 01_basics/04_comparion.js
@@ -0,0 +1,12 @@
+// console.log("2">1);
+// console.log("02">1);
+
+
+// -- avoid this type of conversion
+// console.log(null>0);
+// console.log(0>null);
+// console.log(0==null);
+// console.log(0>=null);
+// console.log(0<=null);
+// console.log(0!=null);
+// -- avoid this type of conversion
---
+++ 01_basics/05_datatypes.js
@@ -0,0 +1,32 @@
+const score=100
+const scorevalue=100.8
+const isloggedin=false
+const outsidetemp=null
+let userEmail
+
+const id=Symbol('123')
+const anotherId=Symbol('123')
+
+// console.log(id)
+// console.log(anotherId)
+// console.log(anotherId===id) // here both value are not same
+ const bigNumber=1246765842993493924802898208298
+// console.log(bigNumber)
+
+
+
+// non-primitive
+const heros=["shaktiman","naagraj","doga"];
+let myobj={
+  name:"medha",
+  age:22,
+}
+
+const myfunction=function(){
+  console.log("hello world");
+}
+// console.log(typeof bigNumber);
+// console.log(typeof score);
+// console.log(typeof outsidetemp);
+// console.log(typeof isloggedin);
+myfunction();
