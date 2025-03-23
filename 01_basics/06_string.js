e381b4955f3f68fbf4b40f09b18deb14d864f3f2
Medha <144618806+smedha99@users.noreply.github.com>
2025-03-23 08:24:40 +0000

    string

---
+++ 01_basics/06_string.js
@@ -0,0 +1,9 @@
+const myname="medha"
+const mypackage="18lpa"
+//console.log(myname+mypackage);  ->odd way
+//console.log(`hii my name is ${myname} and my package is ${mypackage}`);
+
+const gamelover=new String('medha')
+console.log(gamelover[0]);
+console.log(myname[5]);
+console.log(gamelover.toUpperCase());
