6f641e2c06e45bad333a5d1b22589db7a39aff64
Medha <144618806+smedha99@users.noreply.github.com>
2025-03-23 10:32:33 +0000

    array 1

---
+++ 02_basics/01_array.js
@@ -0,0 +1,14 @@
+const myarr=[0,1,2,3,4,5];
+//console.log(myarr);
+myarr.push(9);
+myarr.pop();
+//myarr.pop();
+//console.log(myarr.includes(6));
+//console.log(myarr.indexOf(5));
+console.log("A",myarr);
+const myn1=myarr.slice(1,3);
+console.log(myn1);
+console.log("B",myarr);
+const myn2=myarr.splice(1,3);
+console.log(myn2);
+console.log("C",myarr);
