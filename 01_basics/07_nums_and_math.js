ffd7737b4a2a2408282a0b1c78be51b3481685ee
Medha <144618806+smedha99@users.noreply.github.com>
2025-03-23 09:36:37 +0000

    number and math

---
+++ 01_basics/07_nums_and_math.js
@@ -0,0 +1,21 @@
+// const score =1000;
+// console.log(score);
+// const sc=new Number(400.987);
+// console.log(sc.toFixed(2));
+// console.log(sc.toPrecision(4));
+// console.log(sc.toString());
+
+//++++++++++++++++++++++++++++MATH+++++++++++++++++++++++++++++++++++
+//console.log(Math);
+// console.log(Math.ceil(9.2));
+// console.log(Math.floor(8.6));
+// console.log(Math.round(8.7));
+// console.log(Math.round(8.2));
+// console.log(Math.abs(-5));
+// console.log(Math.max(8,9,7,6));
+// console.log(Math.min(8,9,6,2));
+console.log(Math.random()*10+1);
+const MIN=20;
+const MAX=30;
+console.log(Math.floor(Math.random()*(MAX-MIN+1))+MIN);
+
