6c70fd4b4c78bb7246aecdba7b766f1ab6d1dc8f
Medha <144618806+smedha99@users.noreply.github.com>
2025-03-23 09:36:52 +0000

    dates

---
+++ 01_basics/08_dates.js
@@ -0,0 +1,10 @@
+let d=new Date();
+// console.log(d);
+// console.log(d.toString());
+// console.log(d.toDateString());
+// console.log(d.toISOString());
+// console.log(d.toLocaleDateString());
+
+let mytimestamp=Date.now();
+console.log(mytimestamp);
+console.log(d.getTime());
